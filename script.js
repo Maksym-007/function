520e51dc53ea5a38e395f72ad28dc2d7d16d8158
Maksym-007 <67120578+Maksym-007@users.noreply.github.com>
2020-07-14 17:27:00 +0300

    commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//так задумано чтобы сначало показалось you win! а потом вы вышли из игры! исправляется путем добавления else 
+
+
+const guess = () => {
+    const random = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    let r = random(1, 10);// рандомное число
+    for (let p = true; p != r && p;) {
+        p = prompt('Отгадай число что я загадал от 1 до 10');
+        if (p == r) {
+            alert('You win!');
+        }/*else*/ if (p < r && p) {
+            alert('мое число чуть больше!');
+        } else if (p > r && p) {
+            alert('мое число чуть меньше!');
+        } else
+            alert('Вы вышли из игры!');
+    }
+}
+guess();
